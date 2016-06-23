197fe595571b7508d675764209e90cd61458497f
あまみや ゆうこ <1@1234.sh>
2016-06-23 22:30:28 +0800

    Implement instrument

--- src/TentacleGuitar.Server/Controllers/HomeController.cs
+++ src/TentacleGuitar.Server/Controllers/HomeController.cs
@@ -27,5 +27,26 @@ namespace TentacleGuitar.Server.Controllers
         {
             return Json(DB.Musics.OrderBy(x => x.Level).ToList());
         }
+
+        [HttpPost("/SubmitScore")]
+        public IActionResult SubmitScore(Guid Id, string Token, int Score)
+        {
+            var user = DB.Users.Single(x => x.Token == Token);
+            DB.Histories.Add(new History { MusicId = Id, Point = Score, Time =DateTime.Now, UserId = user.Id });
+            DB.SaveChanges();
+            return Content("OK");
+        }
+
+        [HttpPost("/GetInstrument")]
+        public IActionResult GetInstrument(Guid Id)
+        {
+            var music = DB.Musics.SingleOrDefault(x => x.Id == Id);
+            if (music == null)
+            {
+                Response.StatusCode = 404;
+                return Content("Not Found");
+            }
+            return File(music.Instrument, "application/octet-stream");
+        }
     }
 }
