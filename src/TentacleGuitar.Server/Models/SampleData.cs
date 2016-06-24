51b964be913aede410a2ec50938c18b958cb7735
あまみや ゆうこ <1@1234.sh>
2016-06-24 20:37:31 +0800

    Infrastructures

---
+++ src/TentacleGuitar.Server/Models/SampleData.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace TentacleGuitar.Server.Models
+{
+    public static class SampleData
+    {
+        public static async Task InitDB(IServiceProvider services)
+        {
+            var DB = services.GetRequiredService<GuitarContext>();
+            var UserManager = services.GetRequiredService<UserManager<User>>();
+            var RoleManager = services.GetRequiredService<RoleManager<IdentityRole<long>>>();
+
+            await RoleManager.CreateAsync(new IdentityRole<long>("Root"));
+            await RoleManager.CreateAsync(new IdentityRole<long>("Member"));
+
+            var user = new User { UserName = "root", Email = "1@1234.sh" };
+            await UserManager.CreateAsync(user, "123456");
+
+            await UserManager.AddToRoleAsync(user, "Root");
+        }
+    }
+}
--- src/TentacleGuitar.Server/Startup.cs
+++ src/TentacleGuitar.Server/Startup.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.EntityFrameworkCore;
 using TentacleGuitar.Server.Models;
@@ -16,11 +17,30 @@ namespace TentacleGuitar.Server
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddDbContext<GuitarContext>(x => x.UseSqlite("Data source=tentacleguitar.db"));
+
+            services.AddIdentity<User, IdentityRole>(x =>
+            {
+                x.Password.RequireDigit = false;
+                x.Password.RequiredLength = 0;
+                x.Password.RequireLowercase = false;
+                x.Password.RequireNonAlphanumeric = false;
+                x.Password.RequireUppercase = false;
+                x.User.AllowedUserNameCharacters = null;
+            })
+                 .AddDefaultTokenProviders()
+                 .AddEntityFrameworkStores<GuitarContext>();
+
+            services.AddMvc();
         }
 
-        public void Configure(IApplicationBuilder app)
+        public async void Configure(IApplicationBuilder app)
         {
+            app.UseStaticFiles();
+            app.UseIdentity();
+            app.UseDeveloperExceptionPage();
             app.UseMvcWithDefaultRoute();
+
+            await SampleData.InitDB(app.ApplicationServices);
         }
     }
 }
